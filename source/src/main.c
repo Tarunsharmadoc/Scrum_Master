a1765956864c81640e6b98a34ec6a2cf0f07138a
warmcreators-us <115068126+warmcreators-us@users.noreply.github.com>
2022-10-21 18:00:58 +0530

    added a way to fetch feature data

--- source/src/main.c
+++ source/src/main.c
@@ -3,11 +3,45 @@
 #include <feature.h>
 #include <task.h>
 #include <userStory.h>
-
+#include <stdlib.h>
 int main()
 {
-	// FILE *feature_file=fopen("../external/features.csv","r");
-
+	FILE *feature_file=fopen("../external/features.csv","r");
+    if (feature_file==NULL){
+        printf("Feature File not found");
+        exit(0);
+    }
+	else{
+		char featureFileData[500];
+		fgets(featureFileData,500,feature_file);
+		char* data;
+		feature *f1=(feature*) malloc(sizeof(feature));
+		data=strtok(featureFileData,",");
+		if ( data != NULL )
+    	{
+			f1->featureId=atoi(data);
+    	}
+		data=strtok(NULL,",");
+		if ( data != NULL )
+    	{
+			f1->completionStatus=atoi(data);
+    	}
+		data=strtok(NULL,",");
+		if ( data != NULL )
+    	{
+			strcpy(f1->featureName,data);
+    	}
+		data=strtok(NULL,",");
+		if ( data != NULL )
+    	{
+			strcpy(f1->featureDesc,data);
+    	}
+		printf("%d",f1->featureId);
+		printf("%d",f1->completionStatus);
+		printf("%s",f1->featureName);
+		printf("%s",f1->featureDesc);
+	}
+	fclose(feature_file);
 	printf("\n\t#######################################################");
     printf("\n\t## Welcome to Scrum Sprint Management ###");
     printf("\n\t#######################################################");
